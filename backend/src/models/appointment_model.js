1cd5f67fa143d026cfdab3788aa0698238b3f8b4
springbird123 <yubangkai1998@gmail.com>
2023-07-30 17:37:43 +1000

    merged to user

--- backend/src/models/appointment_model.js
+++ backend/src/models/appointment_model.js
@@ -1,17 +1,13 @@
 const mongoose = require("mongoose");
 
-// Define the Mongoose schema for the "Appointment" entity
 const AppointmentSchema = new mongoose.Schema({
-  staff: { type: mongoose.Schema.Types.ObjectId, ref: "Staff" }, // Property for storing the doctor (expects a string)
-  type: { type: String, required: true }, // Property for storing the type (expects a string)
-  patient: [{ type: mongoose.Schema.Types.ObjectId, ref: "Patient" }], // Property for storing the patient name (expects a string)
-  time: { type: Number, required: true }, // Property for storing the time (expects a string)
-  date: { type: Date, required: true }, // Property for storing the date (expects a string)
-  note: { type: String }, // Property for storing the note (expects a string)
+  practitioner: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // Reference to the User model
+  type: { type: String, required: true },
+  patient: [{ type: mongoose.Schema.Types.ObjectId, ref: "Patient" }], // It may be better to have a single patient for each appointment
+  appointmentDateTime: { type: Date, required: true }, // Combined date and time field
+  note: { type: String }
 });
 
-// Create a Mongoose model named "Appointment" based on the "AppointmentSchema"
 const Appointment = mongoose.model("Appointment", AppointmentSchema);
 
-// Export the "Appointment" model to make it accessible from other parts of the application
 module.exports = Appointment;
--- backend/src/models/user_model.js
+++ backend/src/models/user_model.js
@@ -3,8 +3,12 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   email: { type: String, unique: true, lowercase: true, required: true },
   password: { type: String, required: true },
-  signup: [{ type: mongoose.Schema.Types.ObjectId, ref: "Signup" }], // Reference to a SignUp model
-  // staff: { type: mongoose.Schema.Types.ObjectId, ref: "Staff" },
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  business_name: { type: String, required: true },  // Only required for SignUp, not for Staff
+  title: { type: String, required: false }, // Only required for Staff, not for Signup
+  isAdministrator: { type: Boolean, default: true }, // New field to distinguish an Administrator
+  isPractitioner: { type: Boolean, default: true }, // New field to distinguish a Practitioner
 });
 
 const User = mongoose.model("User", UserSchema);
